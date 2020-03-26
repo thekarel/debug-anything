507f4fce25621a0af3b91816c6c7df71e0a56706
Charles Szilagyi <charles@charlesagile.com>
2020-03-26 08:55:49 +0000

    feat(node): Pick up name from query

--- node/index.js
+++ node/index.js
@@ -1,11 +1,12 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3456;
 const serverUrl = `http://${hostname}:${port}`
 
 const server = http.createServer((req, res) => {
-  const name = 'World'
+  const {name} = url.parse(req.url, true).query;
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
